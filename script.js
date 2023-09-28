ee05adae82b6ce8248d1e003fe2d9be9611126a5
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-29 00:56:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,15 +12,29 @@ function convertToRoman(num) {
   //your code here
 let result = '';
 
-    for (let i = 0; i < values.length; i++) {
-        while (num >= values[i]) {
-            result += symbols[i];
-            num -= values[i];
+     for (let i = 0; i <= 6; i += 2) {
+        const [symbol1, value1] = obj[i];
+        const [symbol2, value2] = obj[i - 1];
+
+        const digit = Math.floor(num / value1);
+        num %= value1;
+
+        if (digit === 9) {
+            result += symbol2 + symbol1;
+        } else if (digit >= 5) {
+            result += symbol1;
+            for (let j = 0; j < digit - 5; j++) {
+                result += symbol2;
+            }
+        } else if (digit === 4) {
+            result += symbol2 + symbol1;
+        } else {
+            for (let j = 0; j < digit; j++) {
+                result += symbol1;
+            }
         }
     }
 
     return result;
-
 }
-
 module.exports = convertToRoman
